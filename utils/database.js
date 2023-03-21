be78bccbb96feef0d5eb819a0a32dae8d50d0b56
aya effoua <carineeffoua@gmail.com>
2023-03-21 16:30:44 -0400

    initializing project

---
+++ utils/database.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql2');
+
+class DbHelper{
+    constructor(){
+       
+    }
+    static async connection(){
+       return new Promise((resolve,reject)=>{
+            try{
+                const connection = mysql.createConnection({
+                    host: 'localhost',
+                    user: 'root',
+                    database: 'test'
+                  });
+                  return resolve(connection)
+            }
+            catch(error){
+                console.error(error.message)
+                reject(error)
+            }
+       })
+    }
+    static async getInstance(){
+        if(!this.instance){
+            try {
+                this.instance= await this.connection()
+            } catch (error) {
+              console.error('cannot be connected to the database',error.message)  
+            }
+            return this.instance
+        }
+    }
+    static async executeQuery(query){
+        return new Promise(async(resolve, reject)=>{
+            try {
+                let pool=await this.getInstance()
+                if (pool){
+                    let data=await pool.query 
+                }
+            } catch (error) {
+                
+            }
+        })
+
+    }
+}
